e8419864b407f27562dc95cb281033ee80d91e6a
SOumitra Maity <maitychottu03225@gmail.com>
2025-11-20 09:37:00 +0530

    Drop down added

--- rdx-toolkit-tsx/src/features/login/loginSlice.ts
+++ rdx-toolkit-tsx/src/features/login/loginSlice.ts
@@ -1,1 +1,46 @@
-import {createSlice} from  '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from  '@reduxjs/toolkit';
+import { useAppSelector, useAppDispatch } from '../../app/Hook';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+interface UserProfile{
+    name:string,
+    email:string,
+    role: "admin" | "teacher" | "student"
+    lastLogin: number | null
+}
+interface AuthState{
+    user: UserProfile |null,
+    isAuthenticated:boolean,
+    loading: 'idle' | 'pending' | 'successed' | 'failed',
+    error: string |null
+}
+
+const initialState:AuthState={
+    user:null,
+    isAuthenticated:false,
+    loading:'idle',
+    error:null
+}
+
+
+
+export const loginfetch= createAsyncThunk( 'login/fetchlogin', async()=>{
+
+
+    const res= await axios.get('')
+})
+
+const loginSlice=createSlice({
+    name: "login",
+    initialState,
+    reducers:{
+
+    },
+    extraReducers: (builder)=>{
+        builder.addCase((state)=>{
+            isloggedIn: true
+        })
+    }
+})
+
